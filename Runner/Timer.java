1f917f84487a21897191a881e5652c61e8bf09b7
Peter-Paik <peter@paikman.com>
2023-07-20 16:32:43 -0500

    continuing timer

--- Runner/Timer.java
+++ Runner/Timer.java
@@ -12,23 +12,58 @@ public class Timer extends JComponent implements Runnable
 {
     private int time;
     private int displayedTime;
+    final private int YPIVOT = 160;
+    final private int XLEFT = 120;
+    final private int XRIGHT = 220;
+    private ArrayList<int[]> numsX = new ArrayList<int[]>();
+    private ArrayList<int[]> numsY = new ArrayList<int[]>();
     public Timer(){
         displayedTime =60;
+        //initialize arraylists
     }
-    //a method for every number
+    
+    /**
+     * only x is different, center y will be the same either way
+     * x is either 146 or 226
+     * YPIVOT is always 160
+     */
+    public void resetTo(int x){
+        //zero
+        //one
+        numsX.get(0)[0]=x+40;numsX.get(0)[1]=x+50;numsX.get(0)[2]=x+50;numsX.get(0)[3]=x+40;
+        numsY.get(0)[0]= YPIVOT+8;numsY.get(0)[1]=YPIVOT+8;
+        numsY.get(0)[2]=YPIVOT+88;numsY.get(0)[3]=YPIVOT+88;
+        
+    }
     
     public void increment(){
         displayedTime++;
     }
     
     public void decrement(){
-        displayedTime--;
+        if(displayedTime!=0)
+            displayedTime--;
     }
     
     public void draw(Graphics2D page){
         Color color = Color.black;
         page.setColor(color);
+        int tensDigit = displayedTime/10;//gets tens digit bc trunkate
+        int onesDigit = displayedTime%10;
         //page.drawPolygon(xs,ys,4);
+        
+        //draing boxes
+        //x -> 6 10s use 4
+        //y -> 12 8s use 10
+        int[] testOneX = {146/*pivot*/+40,146+50,146+50,146+40};
+        int[] testOneY = {160+8,160+8,160+8*11,160+8*11};
+        page.fillPolygon(testOneX,testOneY,4);
+        int[] rectangleX1 = {146,206,206,146};
+        int[] rectangleY1 = {160,160,256,256};
+        int[] rectangleX2 = {226,286,286,226};
+        int[] rectangleY2 = {160,160,256,256};
+        page.drawPolygon(rectangleX1,rectangleY1,4);
+        page.drawPolygon(rectangleX2,rectangleY2,4);
     }
     
     public void setTime(int time){
---
+++ Runner/testing.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class testing here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class testing
+{
+    public static void main(String[] args){
+        int[] numbers = new int[3];
+        int x=0;
+        numbers[0] = 0+x;
+        numbers[1] = 1+x;
+        numbers[2] = 2+x;
+        
+        System.out.println(""+numbers[0] + numbers[1] + numbers[2]);
+        x = 1;
+        numbers[0] = 0+x;
+        numbers[1] = 1+x;
+        numbers[2] = 2+x;
+        System.out.println(""+numbers[0] + numbers[1] + numbers[2]);
+    }
+}
